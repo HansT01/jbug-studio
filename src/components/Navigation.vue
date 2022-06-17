baf3121079cd4f7fab70a5973fb1298378da2a12
Hans Teh <73771314+HansT01@users.noreply.github.com>
2022-06-17 17:05:51 +1000

    Added global css styles

---
+++ src/components/Navigation.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div class="start">
+      <router-link to="/">
+        <img src="@/assets/jbuglogo.png" alt="" class="logo" />
+      </router-link>
+      <router-link to="/about">Home</router-link>
+      <router-link to="/">Home</router-link>
+    </div>
+    <div class="center"></div>
+    <div class="end"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<style scoped></style>
