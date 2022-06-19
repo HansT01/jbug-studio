0bb01ec0d57810a09fc10c5d5ce63c3d52f2186b
Hans Teh <73771314+HansT01@users.noreply.github.com>
2022-06-19 20:01:28 +1000

    Carousel now reads props

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="clr-bg-1">
-    <swiper class="w-full h-64" :slidesPerView="4" :spaceBetween="30" :centeredSlides="true" :modules="[Pagination]">
-      <swiper-slide v-for="item in products" class="flex items-center justify-center clr-bg-0" @click="() => myfunc(item.message)">
-        {{ item.message }}
-        <img :src="item.image" alt="" />
+    <!-- https://swiperjs.com/demos#centered-auto -->
+    <swiper class="w-full" :slidesPerView="'auto'" :spaceBetween="30" :centeredSlides="true" :modules="[Pagination]">
+      <!-- Every slide has a set width and height -->
+      <swiper-slide v-for="item in products" class="slide flex items-center justify-center" @click="() => myfunc(item.message)">
+        <!-- The contained image will do its best to fill up the slide -->
+        <img v-if="item.image" class="object-contain max-w-full max-h-full" :src="item.image" alt="Product image" />
+        <div v-else class="w-full h-full clr-bg-0 flex justify-center items-center">{{ item.message }}</div>
       </swiper-slide>
     </swiper>
   </div>
@@ -13,6 +16,7 @@
 import { Swiper, SwiperSlide } from 'swiper/vue'
 import { Pagination } from 'swiper'
 import 'swiper/scss'
+import 'swiper/scss/pagination'
 
 import { Product } from '~~/composables/test'
 
@@ -26,4 +30,9 @@ defineProps<{
 }>()
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.slide {
+  width: min(450px, 60vw);
+  height: 300px;
+}
+</style>
